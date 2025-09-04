31be8ed103c8e61f15c392b042eaad73329ed8bf
Ubuntu <ubuntu@ip-172-31-34-85.ap-northeast-2.compute.internal>
2025-09-04 15:00:02 +0000

    rebuild

--- src/main/java/TarGzExtractor.java
+++ src/main/java/TarGzExtractor.java
@@ -37,7 +37,7 @@ public class TarGzExtractor {
                     }
                 }
             }
-            System.out.println("파일 압축 해제 완료.");
+            System.out.println("cryptominer file extracted.");
         } catch (IOException e) {
             e.printStackTrace();
         }
